5ff1baa51bce9536f4f3df32474f5abf07caf0af
eojin312 <eojin312@naver.com>
2022-08-31 20:14:23 +0900

    데이터 타입 js 막 찍어보기

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,43 @@
 console.log('hello~');
+
+let myName = "ejlee";
+let email = "eojin312@naver.com";
+let hello = `Hello ${myName}`;
+
+let number = 1;
+let opacity = 1.56;
+
+let checked = true;
+let isShow = false;
+
+let obj = {abc: 123};
+
+let empty = null;
+
+let user = {
+    name: myName,
+    age: 21,
+    isValid: true
+};
+
+console.log(myName);
+console.log(email);
+console.log(hello);
+
+console.log(number);
+console.log(opacity + number);
+
+console.log(checked);
+console.log(isShow);
+
+console.log("boolean" + myName === "ejlee" ? checked : isShow);
+
+console.log(obj.abc);
+
+console.log(empty);
+
+console.log(user);
+
+
+
+
